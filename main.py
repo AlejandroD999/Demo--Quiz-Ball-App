c49cccf2e48b546a76c959994b3305ef492b8016
AlejandroD999 <alejandrodgonzales923@gmail.com>
2025-09-18 11:21:08 -0500

    Added Virtual Environment, and header_frame <- main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import tkinter as tk
-
+import backend
 class QuizGUI(tk.Tk):
 
     def __init__(self):
         super().__init__()
-        self.geometry('400x400')
+        self.geometry('600x400')
         self.title("QuizBall")
 
+        self.widgets()
+
     def widgets(self):
-        pass
+        
+        self.header_frame = tk.Frame(master = self)
+        self.header_frame.pack()
 
 
 root = QuizGUI()
