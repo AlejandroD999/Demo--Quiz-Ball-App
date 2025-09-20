00625352c0e74cbe42e806cf9f3dae6704c0c8b9
AlejandroD999 <alejandrodgonzales923@gmail.com>
2025-09-20 14:39:28 -0500

    Made page swap available. Still in development <- main.py

--- main.py
+++ main.py
@@ -5,22 +5,50 @@ class App(tk.Tk):
 
     def __init__(self):
         super().__init__()
-        self.geometry('600x400')
+        self.geometry("600x400")
         self.title("QuizBall")
 
-        self.widgets()
+        self.container = tk.Frame(self)
+        self.container.pack(fill="both", expand= True)
 
-    def widgets(self):
-        self.current_view = self.home_frame
-        self.home_frame = tk.Frame(master = self)
+        self.pages = {}
+        for Page in (HomePage, ModeSelectionPage, QuizBallPage, ResultsPage):
+            page = Page(self.container, self)
+            self.pages[Page] = page
+            page.grid(row=0, column=0, sticky="nsew")
 
-    def show_home(self):
+        self.show_page(HomePage)
 
-        self.current_view.pack_forget()
-        self.home_frame.pack(self, bg="black")
+    
+    def show_page(self, page_class):
+        page = self.pages[page_class]
+        page.tkraise()
 
-    def show_other(self, current_view):
-        pass
+
+class HomePage(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+        
+        self.config(bg='green', width=600, height=400)
+        self.propagate(False)
+
+        label = tk.Label(self, text="Home Page", fg='white', bg="black").pack(pady=20)
+
+
+class ModeSelectionPage(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent, bg="grey")
+
+        label = tk.Label(self, text="Modes").pack(pady = 20)
+
+class QuizBallPage(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent, bg='red')
+
+class ResultsPage(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent, bg="blue")
 
 
 if __name__ == '__main__':
