630188cff6e7cb3b1f61a7460d177f06131be3e4
AlejandroD999 <alejandrodgonzales923@gmail.com>
2025-09-23 21:28:49 -0500

    Layout of Home Page completed <- main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-import tkinter as tk
+import customtkinter
 import backend
 
-class App(tk.Tk):
+class App(customtkinter.CTk):
 
     def __init__(self):
         super().__init__()
+
         self.geometry("600x400")
         self.title("QuizBall")
+        self.background_color = "#00171f"
 
-        self.container = tk.Frame(self)
+        self.container = customtkinter.CTkFrame(self)
         self.container.pack(fill="both", expand= True)
 
         self.pages = {}
@@ -25,30 +27,43 @@ class App(tk.Tk):
         page.tkraise()
 
 
-class HomePage(tk.Frame):
+class HomePage(customtkinter.CTkFrame):
     def __init__(self, parent, controller):
-        super().__init__(parent)
+
+        super().__init__(parent, fg_color = controller.background_color, corner_radius=0)
         self.controller = controller
-        
-        self.config(bg='green', width=600, height=400)
+
+        self.configure(width=600, height=400)
         self.propagate(False)
 
-        label = tk.Label(self, text="Home Page", fg='white', bg="black").pack(pady=20)
+        app_title = customtkinter.CTkLabel(self, text="Project Name", fg_color=self.controller.background_color, text_color="#007ea7",
+                         font=("Times New Roman", 48)).pack(pady=(35, 0))
 
+        start_button = customtkinter.CTkButton(self, text="Start Quiz", font=("Times New Roman", 25),
+                                 text_color = "black", fg_color = '#007ea7', hover_color = '#00a8e8',
+                                 border_color="black", corner_radius = 3).pack(pady=(100, 0))
+        
+        LearnMore_button = customtkinter.CTkButton(self, text="Learn More", font=("Times New Roman", 25),
+                                 text_color = "black", fg_color = '#007ea7', hover_color = '#00a8e8',
+                                 border_color="black", corner_radius = 3).pack(pady=(20, 0))
 
-class ModeSelectionPage(tk.Frame):
+        exit_button = customtkinter.CTkButton(self, text="Exit", font=("Times New Roman", 25),
+                                 text_color = "black", fg_color = '#007ea7', hover_color = '#00a8e8',
+                                 border_color="black", corner_radius = 3).pack(pady=(20, 0))
+
+class ModeSelectionPage(customtkinter.CTkFrame):
     def __init__(self, parent, controller):
-        super().__init__(parent, bg="grey")
+        super().__init__(parent, fg_color="#003459")
 
-        label = tk.Label(self, text="Modes").pack(pady = 20)
+        label = customtkinter.CTkLabel(self, text="Modes").pack(pady = 20)
 
-class QuizBallPage(tk.Frame):
+class QuizBallPage(customtkinter.CTkFrame):
     def __init__(self, parent, controller):
-        super().__init__(parent, bg='red')
+        super().__init__(parent, fg_color='#003459')
 
-class ResultsPage(tk.Frame):
+class ResultsPage(customtkinter.CTkFrame):
     def __init__(self, parent, controller):
-        super().__init__(parent, bg="blue")
+        super().__init__(parent, fg_color="#003459")
 
 
 if __name__ == '__main__':
